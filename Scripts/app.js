4af2f1a152380687bf909bbad953a49acda2325d
awen-xu <wxu59@my.centennialcollege.ca>
2018-08-01 11:57:04 -0400

    initial commit

---
+++ Scripts/app.js
@@ -0,0 +1,8 @@
+(function(){
+
+    function Start(){
+        console.log(`%cApp started`, "color:blue; font-size:30px, font-weight:bold")
+    }
+
+    window.addEventListener("load", Start);
+})();
